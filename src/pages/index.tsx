d2d626939e585b9880bdb99f963f1596cd770e99
leonardopf26 <leonardopf26@gmail.com>
2021-04-17 14:29:26 -0300

    Added page title to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from "next/Head";
+
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Incío | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+  );
 }
