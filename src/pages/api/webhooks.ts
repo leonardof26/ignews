a81c2aedc04834076b4a807df95961b396d690e1
leonardopf26 <leonardopf26@gmail.com>
2021-04-20 12:05:01 -0300

    Added logic to receive stripe webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -18,10 +20,37 @@ export const config = {
   },
 };
 
+const relevantEvents = new Set(["checkout.session.completed"]);
+
 export default async (req: NextApiRequest, resp: NextApiResponse) => {
+  if (req.method !== "POST") {
+    resp.setHeader("Allow", "POST");
+    return resp.status(405).end("Method not allowed");
+  }
+
   const buf = await buffer(req);
 
   console.log(buf);
 
-  return resp.status(200).json({ ok: true });
+  const secret = req.headers["stripe-signature"];
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      buf,
+      secret,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    return resp.status(400).send(`Webhook error ${err.message}`);
+  }
+
+  const { type } = event;
+
+  if (relevantEvents.has(type)) {
+    console.log("Evento Recebido", event);
+  }
+
+  return resp.json({ received: true });
 };
