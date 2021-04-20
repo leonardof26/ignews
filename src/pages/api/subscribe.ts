3549766a0e294093dd66656a4b197a75a7fdc693
leonardopf26 <leonardopf26@gmail.com>
2021-04-20 07:55:52 -0300

    Created stripe checkout session

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
+import { stripe } from "../../services/stripe";
+
+export default async (req: NextApiRequest, resp: NextApiResponse) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req });
+
+    const stripeCostumer = await stripe.customers.create({
+      email: session.user.email,
+    });
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCostumer.id,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [{ price: "price_1IheX3Evf9T3VZCXSppPtdF5", quantity: 1 }],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return resp.status(200).json({ sessionID: stripeCheckoutSession.id });
+  } else {
+    resp.setHeader("Allow", "POST");
+    resp.status(405).end("Method not allowed");
+  }
+};
