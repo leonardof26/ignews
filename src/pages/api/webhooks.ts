5eb82688db8e29b85716b43a723adfcd79c445f3
leonardopf26 <leonardopf26@gmail.com>
2021-04-20 13:44:43 -0300

    Added logic to update and remove subscriptions

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -21,7 +21,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async (req: NextApiRequest, resp: NextApiResponse) => {
   if (req.method !== "POST") {
@@ -50,12 +54,24 @@ export default async (req: NextApiRequest, resp: NextApiResponse) => {
   if (relevantEvents.has(type)) {
     try {
       switch (type) {
+        case "customer.subscription.updated":
+        case "customer.subscription.deleted":
+          const subscription = event.data.object as Stripe.Subscription;
+
+          await saveSubscription(
+            subscription.id,
+            subscription.customer.toString(),
+            false
+          );
+          break;
+
         case "checkout.session.completed":
           const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
           await saveSubscription(
             checkoutSession.subscription.toString(),
-            checkoutSession.customer.toString()
+            checkoutSession.customer.toString(),
+            true
           );
           break;
 
